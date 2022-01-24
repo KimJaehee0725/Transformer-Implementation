d379db3c4669ce49269fc891e06f8b8d95fe21c7
Kim Jaehee <stapers@naver.com>
2022-01-24 09:06:12 +0000

    dd

--- model_utils.py
+++ model_utils.py
@@ -1,4 +1,3 @@
-from tkinter import Variable
 import torch 
 from torch import nn
 from config import Config
@@ -11,10 +10,10 @@ class EmbeddingLayer(nn.Module):
         super().__init__()
         self.token_embedding = TokenEmbedding(args)
         self.PEembedding = PositionalEmbedding(args)
+        self.embedding = nn.Sequential(self.token_embedding, self.PEembedding)
     
     def forward(self, token_tensor):
-        output = self.token_embedding(token_tensor)
-        output = self.PEembedding(output)
+        output = self.embedding(token_tensor)
         return output
 
 class TokenEmbedding(nn.Module):
@@ -26,7 +25,7 @@ class TokenEmbedding(nn.Module):
 
         self.Embedding_layer = nn.Embedding(
             num_embeddings = self.vocab_size, 
-            embedding_dim = self.model_dim,
+            embedding_dim = self.embed_dim,
             padding_idx = self.pad_idx)
         
     def forward(self, token_tensor):
@@ -41,37 +40,53 @@ class PositionalEmbedding(nn.Module):
         if args.is_sinusoidal == True:
             self.PEEmbedding = self.makeSinusiodalEmbedding(args)
         else :
-            self.PEEmbedding = torch.rand((args.max_len, args.model_dim))
+            self.PEEmbedding = torch.rand((args.max_len, args.embed_dim))
     
     def forward(self, token_embedding):
-        return token_embedding + Variable(self.PEEmbedding, requires_grad = True)
+        return token_embedding + torch.tensor(self.PEEmbedding) #토치 변수 선언(for autograd 이용)
     
     def makeSinusiodalEmbedding(self, args):
-        embedding_tensor = torch.zeros(args.max_len, args.model_dim)
+        embedding_tensor = torch.zeros(args.max_len, args.embed_dim)
 
-        even_max = (args.model_dim + 1)//2
-        odd_max = args.model_dim//2
+        even_max = (args.embed_dim + 1)//2
+        odd_max = args.embed_dim//2
 
         for pos in range(args.max_len):
             pos_even = torch.full(size = (even_max), fill_value = pos)
-            pos_even = [sin(elem/10000**(2*num/args.dim_model)) for num, elem in enumerate(pos_even)]
+            pos_even = [sin(elem/10000**(2*num/args.embed_dim)) for num, elem in enumerate(pos_even)]
             embedding_tensor[pos, 0::2] = pos_even
 
             pos_odd = torch.full(size = (odd_max), fill_value = pos)
-            pos_odd = [cos(elem/10000**(2*num/args.dim_model)) for num, elem in enumerate(pos_odd)]
+            pos_odd = [cos(elem/10000**(2*num/args.embed_dim)) for num, elem in enumerate(pos_odd)]
             embedding_tensor[pos, 1::2] = pos_odd
 
         return embedding_tensor
     
+class EncoderBlock(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.projection = QKVProjectionSublayer(args)
+        self.multihead_attnetion = MultiHeadAttention(args)
+        self.residual_block_1 = ResidualBlockSubLayer(args)
+        self.linear_transformation = LinearTransformSublayer(args)
+        self.residual_block_2 = ResidualBlockSubLayer(args)
+        
 
+    def forward(self, input_seq):
+        Q, K, V = self.projection(input_seq)
+        attention_result = self.multihead_attnetion(Q, K, V)
+        normalized_result_1 = self.residual_block_1(attention_result, V)
+        linear_transformed_result = self.linear_transformation(normalized_result_1)
+        normalized_result_2 = self.residual_block_2(linear_transformed_result)
+        return normalized_result_2
 
-
-
-
-class SelfAttentionLayer(nn.Module):
+class QKVProjectionSublayer(args):
     pass
 
-class FFNNSubLayer(nn.Module):
+class MultiHeadAttention(nn.Module):
+    pass
+
+class LinearTransformSublayer(nn.Module):
     pass
 
 class ResidualBlockSubLayer(nn.Module):
